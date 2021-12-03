800f257f3493e39f37d3eb16b5405c37b66544d1
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-03 22:32:13 +0530

    Added: Load the data of indian state census analyzer

---
+++ main/Indian_StatesCensusAnalyser/Indian_StatesCensusAnalyser/CensusAnalyserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CensusAnalyserException : Exception
+    {
+        
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND, INVALID_FILE_TYPE, INCORRECT_HEADER, INCORRECT_DELIMITER, NO_SUCH_COUNTRY
+        }
+
+        public ExceptionType exceptionType;
+
+        
+        public CensusAnalyserException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
